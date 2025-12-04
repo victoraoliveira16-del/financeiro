0d37171f127a6a42eaccfaaac7e41e5514c1c19d
Victor Oliveira <victor.a.oliveira16@aluno.senai.br>
2025-12-04 16:56:09 -0300

    mexendo com css

--- login.php
+++ login.php
@@ -1,45 +1,45 @@
 <?php
 require_once 'config.php';
-require_once 'mensagem.php';
+require_once 'mensagens.php';
 
-if (isset ($_SESSION['usuario_id'])) {
+if (isset($_SESSION['usuario_id'])) {
     header('Location: index.php');
     exit;
-
-    
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Sistema Financeiro</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <h1>Login - Sistema Financeiro</h1>
 
-    <?php exibir_mensagem() ?>;
+    <?php exibir_mensagem(); ?>
 
     <form action="autenticar.php" method="post">
         <div>
             <label for="email">E-mail:</label>
             <input type="email" name="email" id="email" required>
-</div>
-<div>
-    <label for="senha">Senha:</label>
-    <input type="passoword" name="senha" id="senha" required>
-</div>
+        </div>
+        <div>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha" required>
+        </div>
 
-<div>
-    <button type="submit">Entrar</button>
-</div>
-<form>
-
-<p> Não tem conta? <a href="registro.php">Cadastre-se aqui.</a><p>
-
-
+        <div>
+            <button type="submit"  class="button" id="entrar">Entrar</button>
+        </div>
+    </form>
+    <div class="sem_conta">
+        <p>Não tem conta? <a href="registro.php">Cadastre-se aqui.</a></p>
+    </div>
 </body>
+
 </html>
